e5147d6fb58e266266c1581b275d2b33cf24e33f
Dhaneshwar Kumar <dhannesh@gmail.com>
2026-08-18 14:46:13 +0530

    add saveCart and getCart

--- lab2/crud.js
+++ lab2/crud.js
@@ -1,5 +1,18 @@
 import readline from "readline/promises";
+import { writeFile, readFile } from "fs/promises";
+
 import { stdin, stdout } from "process";
+
+const FILE = "products.json";
+
+const saveCart = async (cart) => {
+  await writeFile(FILE, JSON.stringify(cart, null, 2));
+};
+
+const getCart = async () => {
+  const data = await readFile(FILE, "utf-8");
+  return JSON.parse(data);
+};
 
 const main = async () => {
   const cin = readline.createInterface({ input: stdin, output: stdout });
@@ -27,7 +40,7 @@ const main = async () => {
         break;
       case 5:
         console.log("See you later...😃");
-        process.exit()
+        process.exit();
         break;
       default:
         console.log("Invalid choice! try again 🛑");
